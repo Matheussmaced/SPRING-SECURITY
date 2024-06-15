4934d4eff27df0a183fd0a41f1273acbf9f91ac5
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-14 23:20:27 -0300

    feat: finalizando configuração de banco

--- src/main/java/dio/dio_spring_security/config/WebSecurityConfig.java
+++ src/main/java/dio/dio_spring_security/config/WebSecurityConfig.java
@@ -1,28 +1,41 @@
 package dio.dio_spring_security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import dio.dio_spring_security.config.SecurityDatabaseService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
+  @Autowired
+  private SecurityDatabaseService securityService;
+
+  @Autowired
+  public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(authorizeRequests -> authorizeRequests
             .requestMatchers("/").permitAll()
-            .requestMatchers(HttpMethod.POST, "/login").permitAll() // ROTA PRINCIPAL QUALQUER PESSOA PODE TER ACESSO
+            .requestMatchers(HttpMethod.POST, "/login").permitAll()
             .requestMatchers("/managers").hasRole("MANAGERS")
             .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
             .anyRequest().authenticated())
@@ -33,6 +46,7 @@ public class WebSecurityConfig {
     return http.build();
   }
 
+  // Comentado pois agora estamos usando a base de dados para autenticação
   /*
    * @Bean
    * public UserDetailsService userDetailsService() {
@@ -50,5 +64,5 @@ public class WebSecurityConfig {
    * 
    * return new InMemoryUserDetailsManager(user, admin);
    * }
-   */ // meus dados não estarão mais em memoria
+   */
 }
